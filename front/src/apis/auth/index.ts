5bd27b0d6988d0dd287a7320dd4f79cfae2c5aa7
hhhunnn <gusdk2090@naver.com>
2024-04-26 12:22:17 +0900

    2024.04.26 4교시 1

--- front/src/apis/auth/index.ts
+++ front/src/apis/auth/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { SignInRequestDto } from "./dto/request";
-import { SIGN_IN_REQUEST_URL } from "src/constant";
+import { IdCheckRequestDto, SignInRequestDto } from "./dto/request";
+import { ID_CHECK_REQUEST_URL, SIGN_IN_REQUEST_URL } from "src/constant";
 import { SignInResponseDto } from "./dto/response";
+import ResponseDto from "../response.dto";
 
 // function: 로그인 API 함수
 // 비동기처리 되기때문에 async로 동기처리해줘야함
@@ -12,5 +13,24 @@ export const SignInRequest = async (requestBody: SignInRequestDto) => {
         const responseBody = response.data as SignInResponseDto;
         return responseBody;
     })
-    .catch(error => {})
+    .catch(error => {
+        const responseBody = error.response.data;
+        if (!responseBody) return null;
+        return responseBody as ResponseDto; 
+    });
+    return result;
 }
+// function: 아이디 중복 확인 API 함수
+export const IdCheckRequest = async (requestBody: IdCheckRequestDto) => {
+    const result = await axios.post(ID_CHECK_REQUEST_URL, requestBody)
+    .then(response => {
+        const responseBody = response.data as ResponseDto
+        return responseBody
+    })
+    .catch(error => {
+        const responseBody = error.response.data;
+        if (!responseBody) return null;
+        return responseBody;
+    });
+    return result;
+};
