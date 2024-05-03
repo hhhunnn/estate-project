144b1a0554ceeac1a1deb27250031fe11a1d8a37
hhhunnn <gusdk2090@naver.com>
2024-05-03 14:25:37 +0900

    2024.05.03 오전

--- back/src/main/java/com/estate/back/controller/BoardController.java
+++ back/src/main/java/com/estate/back/controller/BoardController.java
@@ -1,21 +1,22 @@
 package com.estate.back.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estate.back.dto.request.board.PostBoardRequestDto;
 import com.estate.back.dto.response.ResponseDto;
 import com.estate.back.dto.response.board.GetBoardListResponseDto;
+import com.estate.back.dto.response.board.GetSearchBoardListResponseDto;
 import com.estate.back.service.BoardService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/board")
@@ -36,6 +37,14 @@ public class BoardController {
     public ResponseEntity<? super GetBoardListResponseDto> getBoardList() {
         ResponseEntity<? super GetBoardListResponseDto> response = boardService.getBoardList();
 
+        return response;
+    }
+
+    @GetMapping("/list/{searchWord}")
+    public ResponseEntity<? super GetSearchBoardListResponseDto> getSearchBoardList (
+        @PathVariable("searchWord") String searchWord
+    ) {
+        ResponseEntity<? super GetSearchBoardListResponseDto> response = boardService.getSearchBoardList(searchWord);
         return response;
     }
 }
